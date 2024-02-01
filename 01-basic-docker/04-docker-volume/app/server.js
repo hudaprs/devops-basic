3b744fd099d758efb0b177130b3381a80b09a206
Huda Prasetyo <real.hudaprasetyo@gmail.com>
2024-02-01 22:04:02 +0700

    chore: 04-docker-volume

---
+++ 01-basic-docker/04-docker-volume/app/server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const User = require('./User')
+
+const connect = async () => {
+	try {
+		await mongoose.connect(
+			'mongodb://root:example@mongo:27017/docker-kubernetes',
+			{
+				authSource: 'admin'
+			}
+		)
+
+		console.log('mongoose connected')
+	} catch (error) {
+		console.error('mongoose error', error)
+
+		process.exit(1)
+	}
+}
+
+const start = async () => {
+	await connect()
+
+	app.get('/create', async (req, res) => {
+		const user = new User({ name: `Huda Prasetyo 1 - ${Math.random()}` })
+		await user.save()
+
+		res.status(201).json({ message: 'User Created' })
+	})
+
+	app.get('/', (req, res) => {
+		res.json({
+			message: 'Hello From Techworld with Nana'
+		})
+	})
+
+	app.listen(3000, () => console.log(`Server started at port 3000`))
+}
+
+start()
